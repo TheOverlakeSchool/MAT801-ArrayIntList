86f8a3b599faf845192b223454ca94ebdd1b1703
zerbash <dbasham@overlake.org>
2023-09-11 11:38:06 -0700

    9-11-23

--- src/ArrayIntList.java
+++ src/ArrayIntList.java
@@ -1,25 +1,47 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArrayIntList {
 
     private int[] nums;
     private int size;
-    private final int DEFAULT_CAPACITY = 10;
+    private static final int DEFAULT_CAPACITY = 5;
 
     public ArrayIntList() {
-        nums = new int[DEFAULT_CAPACITY];
+        this(DEFAULT_CAPACITY);
     }
 
+    public ArrayIntList(int i) {
+        nums = new int[i];
+    }
+
+
     public void add(int i) {
+        ensureCapacity();
         nums[size] = i;
         size++;
     }
 
+    public void addAll(int[] values) {
+        for (int value : values) {
+            add(value);
+        }
+    }
+
     public void add(int index, int num) {
+        ensureCapacity();
+        for (int i = size; i > index; i--) {
+            nums[i] = nums[i - 1];
+        }
+
         nums[index] = num;
         size++;
     }
 
+    private void ensureCapacity() {
+        if (size == nums.length) {
+            nums = Arrays.copyOf(nums, size * 2);
+        }
+    }
     public String toString() {
         String str = "[";
         if (size > 0) {
--- src/ArrayIntListClient.java
+++ src/ArrayIntListClient.java
@@ -1,10 +1,13 @@
 public class ArrayIntListClient {
     public static void main(String[] args) {
-        ArrayIntList list = new ArrayIntList();
+        ArrayIntList list = new ArrayIntList(11);
         list.add(1);
         list.add(5);
         list.add(3);
-        list.add(3,7);
+        list.add(4);
+        list.add(42);
+        list.addAll(new int[]{3,4,5,6,7});
+        list.add(1,7);
         System.out.println(list);
     }
 }
